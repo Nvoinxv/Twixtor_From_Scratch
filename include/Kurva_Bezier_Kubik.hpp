ededc061401ee6c45a57488b06b6ea11b48c9628
Nvoinxv <sukinvoin67@gmail.com>
2026-08-28 12:25:14 +0800

    penambahan persamaan kurva bazier

---
+++ include/Kurva_Bezier_Kubik.hpp
@@ -0,0 +1,19 @@
+#ifndef KURVA_BEZIER_KUBIK_HPP
+#define KURVA_BEZIER_KUBIK_HPP
+
+#include <cmath>
+
+struct Posisi_Bazier {
+    float x;
+    float y;
+};
+
+struct Posisi_Turunan_Bazier {
+    float dydt;
+    float dxdt;
+};
+
+class Persamaan_Bazier {
+    private:
+
+}
